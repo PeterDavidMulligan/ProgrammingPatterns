9738e912148882f3715ae58fa97166a301d5045b
Peter Mulligan <aezur20@gmail.com>
2017-11-29 05:29:57 +0000

    Initial upload. 'Strategy Pattern' and javadoc documentation.

---
+++ StrategyPattern/src/LeatherArmourStrategy.java
@@ -0,0 +1,11 @@
+
+public class LeatherArmourStrategy implements IArmourStrategy {
+	@Override
+	public double getDefence() {
+		return 12.0;
+	}
+	@Override
+	public double getBlock() {
+		return 5.0;
+	}
+}
